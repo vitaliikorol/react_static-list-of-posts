0a9ad72c09eeefc612f856c4d29027667a2a3ec4
Vitalii Korol <vitaliikorol@github.com>
2020-04-25 18:31:36 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1b6ea50562cf8abf248f73f3d229fa90.js"
+  "./precache-manifest.1e4d9879679de21660c68a59703c4668.js"
 );
 
 self.addEventListener('message', (event) => {
